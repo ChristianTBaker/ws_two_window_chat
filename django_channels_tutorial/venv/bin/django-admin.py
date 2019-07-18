99fde410f443b0683d4304c9d23453e529ef7a89
ChristianTBaker <bakerct90@gmail.com>
2019-07-18 13:54:04 -0500

    removed unnecessary files and made .gitingore

--- django_channels_tutorial/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/christianbaker/Desktop/django_channels_tutorial/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
