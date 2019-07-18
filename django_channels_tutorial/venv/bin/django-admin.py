c44ef385f7a13faac64d1394baa53816427fdf08
ChristianTBaker <bakerct90@gmail.com>
2019-07-18 12:36:59 -0500

    hopefully keeps working

---
+++ django_channels_tutorial/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/christianbaker/Desktop/django_channels_tutorial/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
