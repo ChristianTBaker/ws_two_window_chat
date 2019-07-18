8744493f8259fab53b07ee56dc948b97622f51fa
ChristianTBaker <bakerct90@gmail.com>
2019-07-18 15:56:49 -0500

    chaged consumer to be sycronous

--- django_channels_tutorial/src/chat/consumers.py
+++ django_channels_tutorial/src/chat/consumers.py
@@ -7,8 +7,6 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
@@ -42,6 +40,6 @@ class ChatConsumer(WebsocketConsumer):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        async_to_sync(self.send(text_data=json.dumps({
             'message': message
-        }))
+        })))
